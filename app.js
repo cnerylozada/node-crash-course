31fe7279a2a0e25a433022cd81aa892239e86a74
Cristian <cristian@MacBook-Pro-de-Cristian.local>
2020-07-12 15:30:06 -0500

    feat: environment variable

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port);
 
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
